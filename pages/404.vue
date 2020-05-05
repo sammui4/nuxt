7831f1e96fc09feae721f0622c5b5560f7b42c31
sammui4 <490454041@qq.com>
2020-05-05 11:33:46 +0800

    404

---
+++ pages/404.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>404</div>
+    <div><span v-text="countdown"></span><span>后返回</span><nuxt-link to="/">首页</nuxt-link></div>
+  </div>
+  
+</template>
+<script>
+export default {
+  name:'nofind',
+  layout:'blank',
+  data(){
+    return{
+      countdown:5,
+      timeOut:null,
+    }
+  },
+  created(){
+    this.countDownEvent()
+  },
+  methods:{
+    countDownEvent(){
+      this.timeOut = setInterval(()=>{
+        this.countdown--;
+        if(this.countdown==0){
+          this.$router.push('/');
+          this.timeOut = null;
+        }
+      },500)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
